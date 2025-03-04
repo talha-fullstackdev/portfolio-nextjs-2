8308c1e80b7d7bb9f9f701c20fbfda4c18dafd28
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-04 14:46:16 -0800

    add hover to logo

--- src/app/components/header/Logo.jsx
+++ src/app/components/header/Logo.jsx
@@ -3,7 +3,9 @@ import React from 'react'
 const Logo = () => {
   return (
     <div>
-        <h1 className='text-2xl font-title text-blue-700 dark:text-slate-500 scale-animation'>Talha Nawaz</h1>
+        <h1 className='text-2xl font-title text-blue-700 dark:text-slate-500 scale-animation cursor-pointer '>
+          <a className=" hover:text-blue-400" href="#">Talha Nawaz</a>
+        </h1>
     </div>
   )
 }
