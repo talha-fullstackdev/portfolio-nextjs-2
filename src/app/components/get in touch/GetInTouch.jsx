efc1ad3cbc9d694ae85292185acff25baafd9940
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-04 11:23:12 -0800

    added get in touch section and fill content in it

---
+++ src/app/components/get in touch/GetInTouch.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+const GetInTouch = () => {
+  return (
+    <div>
+      <p>what's next</p>
+      <p>get in touch</p>
+      <p>
+        I am eager to collaborate and expand my knowledge. Whether you have a
+        project to discuss or just want to say hi, my inbox is open for all!
+      </p>
+      <button>say Hello</button>
+    </div>
+  );
+};
+
+export default GetInTouch;
