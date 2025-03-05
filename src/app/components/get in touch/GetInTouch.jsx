9c307fa389cb0ee4804ce7ddb58a012d95b360d1
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-05 11:57:57 -0800

    updated border in button of get in touch section

--- src/app/components/get in touch/GetInTouch.jsx
+++ src/app/components/get in touch/GetInTouch.jsx
@@ -11,7 +11,7 @@ const GetInTouch = () => {
         I am eager to collaborate and expand my knowledge. Whether you have a
         project to discuss or just want to say hi, my inbox is open for all!
       </p>
-      <button className="mt-10 border dark:border-2 dark:hover:text-blue-500 dark:border-1 dark:text-black border-blue-400  text-blue-400 p-2 px-4 rounded-sm hover:text-blue-200"><a href="mailto:nawazt964@gmail.com">say Hello </a></button>
+      <button className="mt-10 border  dark:hover:text-blue-500 dark:border-1 dark:text-black border-blue-400  text-blue-400 p-2 px-4 rounded-sm hover:text-blue-200"><a href="mailto:nawazt964@gmail.com">say Hello </a></button>
     </div>
     </div>
     </AnimatedWrapper>
