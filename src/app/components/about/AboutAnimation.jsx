28a4c3f2fd055d7c0e14cb3cdd8347432050426d
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-06 11:08:25 -0800

    remove the bug from project image section

--- src/app/components/about/AboutAnimation.jsx
+++ src/app/components/about/AboutAnimation.jsx
@@ -1,17 +1,39 @@
-"use client"
-import React from 'react'
-import Lottie from 'lottie-react';
-import aboutAnimation from "../../../assets/images/aboutme.json"
+// "use client"
+// import React from 'react'
+// import Lottie from 'lottie-react';
+// import aboutAnimation from "../../../assets/images/aboutme.json"
+// const AboutAnimation = () => {
+//   return (
+//     <div>
+//     <Lottie 
+//       loading="lazy"
+//       animationData={aboutAnimation} 
+//       className="w-[350px] h-[350px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] object-cover rounded-lg mx-auto sm:mx-0 md:mt-20"
+//     />
+//     </div>
+//   )
+// }
+
+// export default AboutAnimation
+"use client";
+
+import React from "react";
+import dynamic from "next/dynamic";
+import aboutAnimation from "../../../assets/images/aboutme.json";
+
+// Dynamically import Lottie with SSR disabled
+const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
 const AboutAnimation = () => {
   return (
     <div>
-    <Lottie 
-      loading="lazy"
-      animationData={aboutAnimation} 
-      className="w-[350px] h-[350px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] object-cover rounded-lg mx-auto sm:mx-0 md:mt-20"
-    />
+      <Lottie
+        loading="lazy"
+        animationData={aboutAnimation}
+        className="w-[350px] h-[350px] sm:w-[300px] sm:h-[300px] md:w-[400px] md:h-[400px] object-cover rounded-lg mx-auto sm:mx-0 md:mt-20"
+      />
     </div>
-  )
-}
+  );
+};
 
-export default AboutAnimation
+export default AboutAnimation;
--- src/app/components/services/ServicesAnimation.jsx
+++ src/app/components/services/ServicesAnimation.jsx
@@ -13,3 +13,19 @@ const ServicesAnimation = () => {
 }
 
 export default ServicesAnimation
+// import React from "react";
+// import dynamic from "next/dynamic";
+// import animation from "../../../assets/images/Animation.json";
+
+// // Dynamically import Lottie with SSR disabled
+// const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+// const ServicesAnimation = () => {
+//   return (
+//     <div className="">
+//       <Lottie animationData={animation} className="md:w-[500px]" />
+//     </div>
+//   );
+// };
+
+// export default ServicesAnimation;
