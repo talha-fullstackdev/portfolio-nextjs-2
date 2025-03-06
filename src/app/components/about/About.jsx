8ab4964feb03f198b3b600c56e09e44b1712b221
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-06 13:57:14 -0800

    adjusted styles in laptop screen for scrolling

--- src/app/components/about/About.jsx
+++ src/app/components/about/About.jsx
@@ -4,12 +4,14 @@ import AboutAnimation from './AboutAnimation'
 import AnimatedWrapper from '@/app/hooks/AnimatedWrapper'
 const About = () => {
   return (
+    <div id="about">
     <AnimatedWrapper>
-    <div  className=' px-8 pt-14 md:pt-20 md:flex md:justify-around  md:px-10 md:gap-8'>
+    <div  className=' px-8 pt-10 md:pt-10 md:flex md:justify-around  md:px-10 md:gap-8'>
         <AboutMe/>
         <AboutAnimation/>
     </div>
     </AnimatedWrapper>
+    </div>
   )
 }
 
