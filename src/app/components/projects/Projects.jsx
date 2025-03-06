61a6ba597bb80e8c0024177a5d9b36a2143bade4
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-06 14:41:56 -0800

    added animation framer motion to projects

--- src/app/components/projects/Projects.jsx
+++ src/app/components/projects/Projects.jsx
@@ -1,87 +1,215 @@
+// "use client";
+// import React, { useState } from "react";
+// import projectsData from "./data";
+// import Image from "next/image";
+// import AnimatedWrapper from "@/app/hooks/AnimatedWrapper";
+// const Projects = () => {
+//   const [showAll, setShowAll] = useState(false);
+//   const visibleProjects = showAll ? projectsData : projectsData.slice(0, 6);
+
+//   return (
+//     <div id="projects">
+//     <AnimatedWrapper>
+//       <div  className="py-10 px-6 md:px-20 pb-10 md:pb-20 text-white dark:text-slate-500 ">
+//         <h2 className="text-3xl font-semibold mb-6 text-center"   >Projects</h2>
+
+//         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 transition-all duration-500 ease-in-out">
+//           {visibleProjects.map((project) => (
+//             <div
+//               key={project.name}
+//               className="p-4 shadow-2xl transition-transform transform hover:-translate-y-1 hover:shadow-xl rounded-xl"
+//             >
+//               {/* Project Image */}
+//             <a href={project.link} target="_blank">
+//               <div className="relative w-full h-52 overflow-hidden">
+//                 <Image
+//                   src={project.image}
+//                   alt="Project image"
+//                   fill
+//                   sizes="(max-width: 768px) 100vw, 33vw"
+//                   className="rounded-xl cursor-pointer object-cover"
+//                 />
+//               </div>
+//               </a>
+//               {/* Tech Stack */}
+//               <div className="flex items-center justify-between text-gray-400 dark:text-slate-500 text-sm mt-3 md:mt-6">
+//                 <p className="text-slate-400 dark:text-slate-500">
+//                   {project.tech}
+//                 </p>
+//                 <div className="flex gap-2">
+//                   <a
+//                     className="text-lg hover:text-blue-400 cursor-pointer"
+//                     href={project.github}
+//                     target="_blank"
+//                   >
+//                     {project.githubIcon}
+//                   </a>
+//                   <a
+//                     className="text-lg hover:text-blue-400 cursor-pointer"
+//                     href={project.link}
+//                     target="_blank"
+//                   >
+//                     {project.linkIcon}
+//                   </a>
+//                 </div>
+//               </div>
+
+//               {/* Project Name & Year */}
+//               <div className="flex items-center justify-between mt-3">
+//                 <a href={project.link} target="_blank">
+//                   <p className="text-lg font-medium cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-4">
+//                     {project.name}
+//                   </p>
+//                 </a>
+//                 <p className="text-gray-400 dark:text-slate-500">
+//                   {project.date}
+//                 </p>
+//               </div>
+//             </div>
+//           ))}
+//         </div>
+
+//         {/* See More / See Less Button */}
+//         {projectsData.length > 6 && (
+//           <div className="flex justify-center mt-8">
+//             <button
+//               onClick={() => setShowAll(!showAll)}
+//               className="px-6 py-2 border dark:hover:text-blue-500 dark:border-1 dark:text-black border-blue-400 text-blue-400 hover:text-blue-200 rounded-sm transition-all duration-300 md:mt-14"
+//             >
+//               {showAll ? "show Less" : "Show More"}
+//             </button>
+//           </div>
+//         )}
+//       </div>
+//     </AnimatedWrapper>
+//     </div>
+//   );
+// };
+
+// export default Projects;
+/////////////////////////////////////
 "use client";
 import React, { useState } from "react";
+import { motion, AnimatePresence } from "framer-motion";
 import projectsData from "./data";
 import Image from "next/image";
 import AnimatedWrapper from "@/app/hooks/AnimatedWrapper";
+
+const containerVariants = {
+  visible: {
+    opacity: 1,
+    transition: {
+      staggerChildren: 0.2, // Projects appear one by one
+    },
+  },
+  hidden: { opacity: 0 },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 50 }, // Start hidden and below
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: { duration: 0.3, ease: "easeOut" },
+  },
+  exit: {
+    opacity: 0,
+    y: -50, // Move up when disappearing
+    transition: { duration: 0.3, ease: "easeIn" },
+  },
+};
+
 const Projects = () => {
   const [showAll, setShowAll] = useState(false);
   const visibleProjects = showAll ? projectsData : projectsData.slice(0, 6);
 
   return (
     <div id="projects">
-    <AnimatedWrapper>
-      <div  className="py-10 px-6 md:px-20 pb-10 md:pb-20 text-white dark:text-slate-500 ">
-        <h2 className="text-3xl font-semibold mb-6 text-center"   >Projects</h2>
+      <AnimatedWrapper>
+        <div className="py-10 px-6 md:px-20 pb-10 md:pb-20 text-white dark:text-slate-500">
+          <h2 className="text-3xl font-semibold mb-6 text-center">Projects</h2>
 
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4 transition-all duration-500 ease-in-out">
-          {visibleProjects.map((project) => (
-            <div
-              key={project.name}
-              className="p-4 shadow-2xl transition-transform transform hover:-translate-y-1 hover:shadow-xl rounded-xl"
-            >
-              {/* Project Image */}
-            <a href={project.link} target="_blank">
-              <div className="relative w-full h-52 overflow-hidden">
-                <Image
-                  src={project.image}
-                  alt="Project image"
-                  fill
-                  sizes="(max-width: 768px) 100vw, 33vw"
-                  className="rounded-xl cursor-pointer object-cover"
-                />
-              </div>
-              </a>
-              {/* Tech Stack */}
-              <div className="flex items-center justify-between text-gray-400 dark:text-slate-500 text-sm mt-3 md:mt-6">
-                <p className="text-slate-400 dark:text-slate-500">
-                  {project.tech}
-                </p>
-                <div className="flex gap-2">
-                  <a
-                    className="text-lg hover:text-blue-400 cursor-pointer"
-                    href={project.github}
-                    target="_blank"
-                  >
-                    {project.githubIcon}
+          <motion.div
+            className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-4"
+            variants={containerVariants}
+            initial="hidden"
+            animate="visible"
+          >
+            <AnimatePresence>
+              {visibleProjects.map((project) => (
+                <motion.div
+                  key={project.name}
+                  className="p-4 shadow-2xl transition-transform transform hover:-translate-y-1 hover:shadow-xl rounded-xl"
+                  variants={itemVariants}
+                  initial="hidden"
+                  animate="visible"
+                  exit="exit"
+                >
+                  {/* Project Image */}
+                  <a href={project.link} target="_blank">
+                    <div className="relative w-full h-52 overflow-hidden">
+                      <Image
+                        src={project.image}
+                        alt="Project image"
+                        fill
+                        sizes="(max-width: 768px) 100vw, 33vw"
+                        className="rounded-xl cursor-pointer object-cover"
+                      />
+                    </div>
                   </a>
-                  <a
-                    className="text-lg hover:text-blue-400 cursor-pointer"
-                    href={project.link}
-                    target="_blank"
-                  >
-                    {project.linkIcon}
-                  </a>
-                </div>
-              </div>
 
-              {/* Project Name & Year */}
-              <div className="flex items-center justify-between mt-3">
-                <a href={project.link} target="_blank">
-                  <p className="text-lg font-medium cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-4">
-                    {project.name}
-                  </p>
-                </a>
-                <p className="text-gray-400 dark:text-slate-500">
-                  {project.date}
-                </p>
-              </div>
+                  {/* Tech Stack */}
+                  <div className="flex items-center justify-between text-gray-400 dark:text-slate-500 text-sm mt-3 md:mt-6">
+                    <p className="text-slate-400 dark:text-slate-500">
+                      {project.tech}
+                    </p>
+                    <div className="flex gap-2">
+                      <a
+                        className="text-lg hover:text-blue-400 cursor-pointer"
+                        href={project.github}
+                        target="_blank"
+                      >
+                        {project.githubIcon}
+                      </a>
+                      <a
+                        className="text-lg hover:text-blue-400 cursor-pointer"
+                        href={project.link}
+                        target="_blank"
+                      >
+                        {project.linkIcon}
+                      </a>
+                    </div>
+                  </div>
+
+                  {/* Project Name & Year */}
+                  <div className="flex items-center justify-between mt-3">
+                    <a href={project.link} target="_blank">
+                      <p className="text-lg font-medium cursor-pointer hover:text-blue-400 hover:underline hover:underline-offset-4">
+                        {project.name}
+                      </p>
+                    </a>
+                    <p className="text-gray-400 dark:text-slate-500">
+                      {project.date}
+                    </p>
+                  </div>
+                </motion.div>
+              ))}
+            </AnimatePresence>
+          </motion.div>
+
+          {/* Show More / Show Less Button */}
+          {projectsData.length > 6 && (
+            <div className="flex justify-center mt-8">
+              <button
+                onClick={() => setShowAll(!showAll)}
+                className="px-6 py-2 border dark:hover:text-blue-500 dark:border-1 dark:text-black border-blue-400 text-blue-400 hover:text-blue-200 rounded-sm transition-all duration-300 md:mt-14"
+              >
+                {showAll ? "Show Less" : "Show More"}
+              </button>
             </div>
-          ))}
+          )}
         </div>
-
-        {/* See More / See Less Button */}
-        {projectsData.length > 6 && (
-          <div className="flex justify-center mt-8">
-            <button
-              onClick={() => setShowAll(!showAll)}
-              className="px-6 py-2 border dark:hover:text-blue-500 dark:border-1 dark:text-black border-blue-400 text-blue-400 hover:text-blue-200 rounded-sm transition-all duration-300 md:mt-14"
-            >
-              {showAll ? "show Less" : "Show More"}
-            </button>
-          </div>
-        )}
-      </div>
-    </AnimatedWrapper>
+      </AnimatedWrapper>
     </div>
   );
 };
--- src/app/components/services/ServicesDetails.jsx
+++ src/app/components/services/ServicesDetails.jsx
@@ -1,43 +1,124 @@
+// import React from "react";
+// import data from "./data";
+// import Image from "next/image";
+
+// const ServicesDetails = () => {
+//   return (
+//     <div className="mt-10 px-6 flex flex-col gap-8">
+//       <h2 className="text-center text-3xl font-semibold text-gray-400 dark:text-slate-500">
+//         Full Stack Development
+//       </h2>
+
+//       <div className="mt-6 flex flex-wrap justify-center gap-8 md:w-[600px]">
+//         {data.map((item, index) => (
+//           <div
+//             key={index}
+//             className="relative group flex cursor-pointer items-center justify-center w-16 h-16 dark:bg-white bg-slate-600 rounded-full shadow-md p-2"
+//           >
+//             <Image
+//               src={item.icon}
+//               alt="image"
+//               title={item.tooltip}
+//               width={40}
+//               height={40}
+//             />
+
+//             <span className="absolute bottom-full mb-2 hidden w-max max-w-[200px] bg-gray-800 dark:text-white text-blue-400 text-xs font-medium px-3 py-2 rounded-md shadow-lg group-hover:block transition-opacity duration-300">
+//               {item.tooltip}
+//             </span>
+//           </div>
+//         ))}
+//       </div>
+
+//       <div className="mt-6 text-slate-400 dark:text-slate-500 md:mt-4 ">
+//         <p className="mt-2">
+//           Building responsive static/dynamic websites using Next.js
+//         </p>
+//         <p className="mt-2">Building responsive Single Page Apps in React.js</p>
+//         <p className="mt-2">Converting Figma designs into webpages</p>
+//       </div>
+//     </div>
+//   );
+// };
+
+// export default ServicesDetails;
+"use client"
 import React from "react";
+import { motion } from "framer-motion";
 import data from "./data";
 import Image from "next/image";
 
+const containerVariants = {
+  hidden: { opacity: 0 },
+  visible: {
+    opacity: 1,
+    transition: {
+      duration: 0.8,
+      ease: "easeInOut",
+      staggerChildren: 0.2, // Staggered animation
+    },
+  },
+};
+
+const itemVariants = {
+  hidden: { opacity: 0, y: 50 }, // Slide up effect
+  visible: {
+    opacity: 1,
+    y: 0,
+    transition: {
+      duration: 0.5,
+      ease: "easeOut",
+    },
+  },
+};
+
 const ServicesDetails = () => {
   return (
-    <div className="mt-10 px-6 flex flex-col gap-8">
+    <motion.div
+      className="mt-10 px-6 flex flex-col gap-8"
+      variants={containerVariants}
+      initial="hidden"
+      whileInView="visible"
+      viewport={{ once: true, amount: 0.2 }} // Trigger when 20% is in view
+    >
       <h2 className="text-center text-3xl font-semibold text-gray-400 dark:text-slate-500">
         Full Stack Development
       </h2>
 
-      <div className="mt-6 flex flex-wrap justify-center gap-8 md:w-[600px]">
+      <motion.div
+        className="mt-6 flex flex-wrap justify-center gap-8 md:w-[600px]"
+        variants={containerVariants} // Apply staggered animation
+      >
         {data.map((item, index) => (
-          <div
+          <motion.div
             key={index}
             className="relative group flex cursor-pointer items-center justify-center w-16 h-16 dark:bg-white bg-slate-600 rounded-full shadow-md p-2"
+            variants={itemVariants} // Apply item animation
           >
-            <Image
-              src={item.icon}
-              alt="image"
-              title={item.tooltip}
-              width={40}
-              height={40}
-            />
+            <motion.div
+              whileHover={{ scale: 1.1, rotate: 6, transition: { duration: 0.3 } }} // Hover effect
+            >
+              <Image src={item.icon} alt="image" title={item.tooltip} width={40} height={40} />
+            </motion.div>
 
-            <span className="absolute bottom-full mb-2 hidden w-max max-w-[200px] bg-gray-800 dark:text-white text-blue-400 text-xs font-medium px-3 py-2 rounded-md shadow-lg group-hover:block transition-opacity duration-300">
+            {/* Tooltip Animation */}
+            <motion.span
+              className="absolute bottom-full mb-2 hidden w-max max-w-[200px] bg-gray-800 dark:text-white text-blue-400 text-xs font-medium px-3 py-2 rounded-md shadow-lg group-hover:block transition-opacity duration-300"
+              initial={{ opacity: 0, y: -10 }}
+              whileHover={{ opacity: 1, y: 0, transition: { duration: 0.3 } }}
+            >
               {item.tooltip}
-            </span>
-          </div>
+            </motion.span>
+          </motion.div>
         ))}
-      </div>
+      </motion.div>
 
       <div className="mt-6 text-slate-400 dark:text-slate-500 md:mt-4 ">
-        <p className="mt-2">
-          Building responsive static/dynamic websites using Next.js
-        </p>
+        <p className="mt-2">Building responsive static/dynamic websites using Next.js</p>
         <p className="mt-2">Building responsive Single Page Apps in React.js</p>
         <p className="mt-2">Converting Figma designs into webpages</p>
       </div>
-    </div>
+    </motion.div>
   );
 };
 
