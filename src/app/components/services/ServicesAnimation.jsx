54806315a955f0ec62a6d3a54778fedac51e5b95
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-06 11:45:24 -0800

    remove services animation for deployment

--- src/app/components/services/ServicesAnimation.jsx
+++ src/app/components/services/ServicesAnimation.jsx
@@ -1,26 +1,100 @@
-"use client"
-import React from 'react'
-import Lottie from "lottie-react";
-import animation from "../../../assets/images/Animation.json"
-const ServicesAnimation = () => {
-  return (
+// "use client"
+// import React from 'react'
+// import Lottie from "lottie-react";
+// import animation from "../../../assets/images/Animation.json"
+// const ServicesAnimation = () => {
+//   return (
   
-    <div className="">
-      <Lottie animationData={animation} className="md:w-[500px]" />
-    </div>
+//     <div className="">
+//       <Lottie animationData={animation} className="md:w-[500px]" />
+//     </div>
     
-  )
-}
+//   )
+// }
 
-export default ServicesAnimation
-// import React from "react";
+// export default ServicesAnimation
+// // import React from "react";
+// // import dynamic from "next/dynamic";
+// // import animation from "../../../assets/images/Animation.json";
+
+// // // Dynamically import Lottie with SSR disabled
+// // const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+// // const ServicesAnimation = () => {
+// //   return (
+// //     <div className="">
+// //       <Lottie animationData={animation} className="md:w-[500px]" />
+// //     </div>
+// //   );
+// // };
+
+// // export default ServicesAnimation;
+// // "use client"; // Ensures this component only runs on the client side
+
+// // import React from "react";
+// // import dynamic from "next/dynamic";
+
+// // // Dynamically import Lottie with SSR disabled
+// // const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+// // import animation from "../../../assets/images/Animation.json";
+
+// // const ServicesAnimation = () => {
+// //   return (
+// //     <div className="">
+// //       <Lottie animationData={animation} className="md:w-[500px]" />
+// //     </div>
+// //   );
+// // };
+
+// // export default ServicesAnimation;
+// // "use client";
+
+// // import React, { useEffect, useState } from "react";
+// // import dynamic from "next/dynamic";
+
+// // // Dynamically import Lottie to disable SSR
+// // const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
+
+// // import animation from "../../../assets/images/Animation.json";
+
+// // const ServicesAnimation = () => {
+// //   const [isClient, setIsClient] = useState(false);
+
+// //   // Ensure this component only renders after mounting (avoiding hydration errors)
+// //   useEffect(() => {
+// //     setIsClient(true);
+// //   }, []);
+
+// //   return (
+// //     <div className="">
+// //       {isClient && <Lottie animationData={animation} className="md:w-[500px]" />}
+// //     </div>
+// //   );
+// // };
+
+// // export default ServicesAnimation;
+// "use client";
+
+// import React, { useEffect, useState } from "react";
 // import dynamic from "next/dynamic";
-// import animation from "../../../assets/images/Animation.json";
 
-// // Dynamically import Lottie with SSR disabled
+// // Dynamically import Lottie to disable SSR
 // const Lottie = dynamic(() => import("lottie-react"), { ssr: false });
 
+// import animation from "../../../assets/images/Animation.json";
+
 // const ServicesAnimation = () => {
+//   const [isClient, setIsClient] = useState(false);
+
+//   useEffect(() => {
+//     setIsClient(true);
+//   }, []);
+
+//   if (!isClient) {
+//     return null; // Don't render anything on the server
+//   }
+
 //   return (
 //     <div className="">
 //       <Lottie animationData={animation} className="md:w-[500px]" />
