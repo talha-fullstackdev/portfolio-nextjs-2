05fcec06fb10a73fe3997e87450ead407ee53cce
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-07 15:03:35 -0800

    add separate data file for footer links

---
+++ src/app/components/footer/Data.jsx
@@ -0,0 +1,33 @@
+import { FiGithub } from "react-icons/fi";
+import { FaInstagram } from "react-icons/fa";
+import { SlSocialTwitter } from "react-icons/sl";
+import { FaLinkedin } from "react-icons/fa6";
+import { FaFacebook } from "react-icons/fa";
+const data = [
+    {
+      name:"github",
+      icon :<FiGithub />,
+      link:"https://github.com/talha-fullstackdev?tab=overview&from=2025-02-01&to=2025-02-07"
+    },
+    {
+      name:"instagram",
+      icon :<FaInstagram />,
+      link:"https://www.instagram.com/"
+    },
+    {
+      name:"twitter-X",
+      icon :<SlSocialTwitter />,
+      link:"https://x.com/?lang=en"
+    },
+    {
+      name:"linkedin",
+      icon :<FaLinkedin />,
+      link:"https://www.linkedin.com/in/talha-nawaz-5421931a4/"
+    },
+    {
+      name:"facebook",
+      icon :<FaFacebook />,
+      link:"https://www.facebook.com/"
+    },
+  ]
+export default data
--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -1,38 +1,8 @@
 import React from 'react'
-import { FiGithub } from "react-icons/fi";
-import { FaInstagram } from "react-icons/fa";
-import { SlSocialTwitter } from "react-icons/sl";
-import { FaLinkedin } from "react-icons/fa6";
-import { FaFacebook } from "react-icons/fa";
+import data from './Data';
 import AnimatedWrapper from '@/app/hooks/AnimatedWrapper';
 const Footer = () => {
-  const data = [
-    {
-      name:"github",
-      icon :<FiGithub />,
-      link:"https://github.com/talha-fullstackdev?tab=overview&from=2025-02-01&to=2025-02-07"
-    },
-    {
-      name:"instagram",
-      icon :<FaInstagram />,
-      link:"https://www.instagram.com/"
-    },
-    {
-      name:"twitter-X",
-      icon :<SlSocialTwitter />,
-      link:"https://x.com/?lang=en"
-    },
-    {
-      name:"linkedin",
-      icon :<FaLinkedin />,
-      link:"https://www.linkedin.com/in/talha-nawaz-5421931a4/"
-    },
-    {
-      name:"facebook",
-      icon :<FaFacebook />,
-      link:"https://www.facebook.com/"
-    },
-  ]
+
   return (
     <AnimatedWrapper>    <div className=" dark:text-slate-500 pt-32 "> 
     <div className='flex justify-center gap-7 max-w-[600px] px-10 m-auto '>
