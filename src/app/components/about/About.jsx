54d2dea2ed2401a20ddf91c397bbc47cc9adbae6
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-10 12:12:53 -0800

    added padding from top to about section

--- src/app/components/about/About.jsx
+++ src/app/components/about/About.jsx
@@ -6,7 +6,7 @@ const About = () => {
   return (
     <div id="about">
     <AnimatedWrapper>
-    <div  className=' px-8 pt-10 md:pt-16 md:flex md:justify-around  md:px-10 md:gap-8'>
+    <div  className=' px-8 pt-20 md:pt-16 md:flex md:justify-around  md:px-10 md:gap-8'>
         <AboutMe/>
         <AboutAnimation/>
     </div>
