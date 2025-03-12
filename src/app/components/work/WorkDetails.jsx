d341da9dc11c91471f512b7c7c1aeb95a1bdc016
talha-fullstackdev <talhafullstack786@gmail.com>
2025-03-12 10:45:35 -0800

    adjusted styles in work details to remove horizontal scroll effect on mobile screen

--- src/app/components/work/WorkDetails.jsx
+++ src/app/components/work/WorkDetails.jsx
@@ -12,13 +12,13 @@ const WorkDetails = () => {
 
   return (
     <AnimatedWrapper>
-    <div className="px-10 md:px-12 md:pt-32 md:pb-20  pt-20" id="experience">
+    <div className="px-4 md:px-12 md:pt-32 md:pb-20  pt-20" id="experience">
       <h2 className="text-2xl  md:text-3xl text-center font-semibold text-white dark:text-slate-500 mb-6 md:tracking-wide">
         Where I've Worked
       </h2>
 
-      <div className="flex pt-4 md:pt-0 flex-col md:flex-row md:justify-center md:gap-6 flex-wrap">
-        <div className="flex gap-2 md:flex-col  md:space-x-0 md:space-y-4 mb-4 md:mb-0">
+      <div className="flex px-4 pt-4 md:pt-0 flex-col md:flex-row md:justify-center md:gap-6 flex-wrap">
+        <div className="flex gap-1 justify-center md:justify-start md:flex-col  md:space-x-0 md:space-y-4 mb-4 md:mb-0">
           {data.map((item) => (
             <button
               key={item.id}
