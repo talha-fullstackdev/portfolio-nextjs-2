6a16156c7099ead6e2664d10fe1adaea0b79669c
talha-fullstackdev <talhafullstack786@gmail.com>
2025-04-23 15:21:02 +0500

    adjust the contact scroll

--- src/app/components/footer/Footer.jsx
+++ src/app/components/footer/Footer.jsx
@@ -7,13 +7,15 @@ const Footer = () => {
     <AnimatedWrapper>    <div className=" dark:text-slate-500 pt-32 "> 
     <div className='flex justify-center gap-7 max-w-[600px] px-10 m-auto '>
        {data.map((item)=>(
-        <div key={item.name} className=" hover:rotate-[360deg] transition-all duration-1000">
+        <div id='contact' key={item.name} className=" hover:rotate-[360deg] transition-all duration-1000 ">
           <a className='text-2xl cursor-pointer dark:hover:text-blue-500 hover:text-blue-400' target='_blank' title={item.name} href={item.link}>{item.icon}</a>
         </div>
        ))}
         
     </div>
+ 
     <p className='mt-6 text-[16px] text-slate-400 dark:text-slate-500 text-center pb-10'>Design & Built By <a href="https://www.linkedin.com/in/talha-nawaz-5421931a4/" target='_blank' className='text-blue-500 hover:text-blue-300'>Talha Nawaz</a> </p>
+ 
     </div>
     </AnimatedWrapper>
 
--- src/app/components/get in touch/GetInTouch.jsx
+++ src/app/components/get in touch/GetInTouch.jsx
@@ -3,15 +3,15 @@ import AnimatedWrapper from "@/app/hooks/AnimatedWrapper";
 const GetInTouch = () => {
   return (
     <AnimatedWrapper>
-    <div id="contact">
+    <div >
     <div className="px-10 text-center pt-28 max-w-[700px] m-auto md:pb-20 pb-2 ">
       <p className="text-blue-400  md:text-xl">what's Next</p>
-      <p className="mt-4  md:text-4xl  dark:text-slate-500 text-white text-2xl font-semibold md:font-bold">Get In Touch</p>
-      <p className="mt-4 text-slate-400 dark:text-slate-500 md:text-lg">
+      <p className="mt-4  md:text-4xl  dark:text-slate-500 text-white text-2xl font-semibold md:font-bold ">Get In Touch</p>
+      <p className="mt-4 text-slate-400 dark:text-slate-500 md:text-lg ">
         I am eager to collaborate and expand my knowledge. Whether you have a
         project to discuss or just want to say hi, my inbox is open for all!
       </p>
-      <button className="mt-10 border  dark:hover:text-blue-500 dark:border-1 hover:rotate-12 transition-all duration-500 dark:text-black border-blue-400  text-blue-400 p-2 px-4 rounded-sm hover:text-blue-200"><a href="mailto:nawazt964@gmail.com">say Hello </a></button>
+      <button className="mt-10 border  dark:hover:text-blue-500 dark:border-1 hover:rotate-12 transition-all duration-500 dark:text-black border-blue-400  text-blue-400 p-2 px-4 rounded-sm hover:text-blue-200"><a href="mailto:nawazt964@gmail.com ">say Hello </a></button>
     </div>
     </div>
     </AnimatedWrapper>
